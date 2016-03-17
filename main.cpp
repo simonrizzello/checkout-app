1b000b3c2327a6b7b417b7f5811b1175eba2a7db
Simon Rizzello <simon.rizzello@sky.uk>
2016-03-17 12:30:12 +0000

    Pattern match demo on input file.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
+#include <fstream>
 #include <utility>
 #include <map>
+#include <regex>
 #include "Item.h"
 
 using namespace std;
 
-const string PRODUCT_LIST = "products_list.txt";
+//const string PRODUCT_LIST = "products_list.txt";
 
 int main() {
 
 	cout	<< "Hello, World!" << endl;
 
-	cout << "Reading " << PRODUCT_LIST << endl;
+	cout << "Reading " << "products_list.txt" << endl;
+
+	regex price_line("^([0-9]+)\\s+\\|\\s*(\\D+)\\|\\s*£([0-9]+\\.[0-9]+)");
+	//regex price_line(R"^(\d+)\s+|\s*(\D+)|\s*£(\d+\.\d+)");
+	string line;
+	ifstream infile("products_list.txt");
+	if (infile.is_open()) {
+		while (getline(infile,line)) {
+			smatch m;
+			// cout << line << endl; 
+			regex_match(line, m, price_line);
+			for (auto result : m) {
+				cout << "[" << result << "]" << endl;
+			}
+		}
+		infile.close();
+	}
+
+	
 
 	map<const int, pair<const string, const double>> products;
 	pair<const string, const double> prod1("Chocolate Cake", 9.25);
