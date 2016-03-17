8a2bf0ab0088c7f93b2de1dc12b5726b9bbfadb8
Simon Rizzello <simon.rizzello@sky.uk>
2016-03-17 12:51:34 +0000

    Used regex to load file into product list.

--- main.cpp
+++ main.cpp
@@ -15,30 +15,27 @@ int main() {
 
 	cout << "Reading " << "products_list.txt" << endl;
 
+	map<const int, pair<const string, const double>> products;
+
 	regex price_line("^([0-9]+)\\s+\\|\\s*(\\D+)\\|\\s*£([0-9]+\\.[0-9]+)");
-	//regex price_line(R"^(\d+)\s+|\s*(\D+)|\s*£(\d+\.\d+)");
 	string line;
 	ifstream infile("products_list.txt");
 	if (infile.is_open()) {
 		while (getline(infile,line)) {
 			smatch m;
 			// cout << line << endl; 
-			regex_match(line, m, price_line);
+			if (regex_match(line, m, price_line)) {
 			for (auto result : m) {
 				cout << "[" << result << "]" << endl;
-			}
+				pair<const string, const double> 
+					p(m[2], stod(m[3]));
+				products.insert(pair<int, pair<string, double>>(stoi(m[1]),p));
+			}} else { cout << "no match" << endl; }
+
 		}
 		infile.close();
 	}
-
 	
-
-	map<const int, pair<const string, const double>> products;
-	pair<const string, const double> prod1("Chocolate Cake", 9.25);
-	products.insert(pair<const int, pair<const string, const double>>
-			(1, prod1));
-
-
 	return 0;
 }
 
