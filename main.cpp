1f7cbd1d1d6cd6e95c74d553a2669445b8dd067f
Simon Rizzello <simon.rizzello@sky.uk>
2016-03-17 11:50:30 +0000

    Vector functionality testing.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <utility>
+#include <map>
+#include "Item.h"
 
 using namespace std;
 
+const string PRODUCT_LIST = "products_list.txt";
+
 int main() {
 
-	cout << "Hello, World!" << endl;
+	cout	<< "Hello, World!" << endl;
+
+	cout << "Reading " << PRODUCT_LIST << endl;
+
+	map<const int, pair<const string, const double>> products;
+	pair<const string, const double> prod1("Chocolate Cake", 9.25);
+	products.insert(pair<const int, pair<const string, const double>>
+			(1, prod1));
+
 
 	return 0;
 }
