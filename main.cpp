6555a16d2fa45a7f5c4d1db0adec8778b09d1ba5
Simon Rizzello <simon.rizzello@sky.uk>
2016-03-22 10:56:53 +0000

    Added functionality to check current promotional offers in promotional rules lambda function.

--- main.cpp
+++ main.cpp
@@ -14,17 +14,36 @@ int main() {
 
 	cout << "Reading " << "products_list.txt" << endl;
 
-	//map<int, pair<int,double>> items_on_promotion;
-	//items_on_promotion.insert(1, pair<int, double> p(2, 9.25));
+	// Create promotions list and add offer for chocolate cake.
+	using offer_t = pair<int,double>;
+	using product_code_t = int;
+	using promotions_list_t = map<product_code_t, offer_t>;
+	promotions_list_t promotions_list;
+	offer_t offer(2, 8.50);
+	promotions_list.insert(pair<product_code_t, offer_t>(1, offer));
+
+	const double discount_threshold = 60.0;
 
 	// The function to add up the basket is implemented here as a lambda function.
 	// In this way it is easy to configure the API to change the promotional rules.
 	function<double(map<int, pair<string, double>>, vector<int>)> promotional_rules =
-		[](map<int, pair<string, double>> prods, vector<int> bskt) {
+		[&promotions_list, discount_threshold]
+		(map<int, pair<string, double>> prods, vector<int> bskt) {
+			promotions_list_t::iterator it;
 			double result = 0;
 			for (auto item : bskt) {
-				result += prods.find(item)->second.second;
+				// Check item against promotions list.
+				it = promotions_list.find(item);
+				if (it != promotions_list.end() &&
+				    count(bskt.begin(), bskt.end(), item) >= it->second.first)
+					result += it->second.second;
+				 else 
+					result += prods.find(item)->second.second;
 			}
+
+			if (result >= discount_threshold)
+				result = result * 0.9;
+
 			return result;
 		};
 
